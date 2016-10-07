bb67c6408e738c8fb2937d0aace41d5c83eb4935
Unshiar <sergosr.ryab@yandex.ru>
2016-10-07 10:55:21 +0300

    Add some methods and logic to CourseStatus class

--- JavaCM/src/javacm/CourseStatus.java
+++ JavaCM/src/javacm/CourseStatus.java
@@ -5,6 +5,7 @@
  */
 package javacm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,12 +14,56 @@ import java.util.List;
  */
 public class CourseStatus {
     
-    private List<String> score;
+    //поля
+    private String listenCurse;
+    private List score = new ArrayList();
 
     //конструкторы
     public CourseStatus() {
         
     }
+
+    public CourseStatus(String listenCurse) {
+        this.listenCurse = listenCurse;
+    }
     
+    
+    //методы
+    public String getListenCurse() {
+        
+        return listenCurse;
+    }
+
+    public void setListenCurse(String listenCurse) {
+        
+        this.listenCurse = listenCurse;
+    }
+
+    public void setScore(float score) {
+        
+        this.score.add(score);
+    }
+    
+    //текуцщий средний балл
+    public float getAverageScore() {
+        
+        float sum = 0;
+        int i, total;
+        
+        total = score.size();
+        if(total == 0){
+            return 0;
+        }
+        for(i=0;i<total;i++) {
+            sum += (float)score.get(i);
+        }
+        return sum/1;
+    }
+    
+    //финальная оценка
+    public int getFinalScore() {
+        
+        return (int)(0.5f+getAverageScore());
+    }
     
 }
