0f58e6b7882a18f41833c8f01a22fdc5ffe4febd
shrek301 <tim_lat@mail.ru>
2019-01-16 09:53:45 +0000

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS Kurs
   FOREIGN KEY (professor_id) REFERENCES Professor (id),
   name         VARCHAR(50) NOT NULL COMMENT 'Наименование',
   number       INTEGER(50) COMMENT 'Номер',
-  sale         FLOAT(5) COMMENT 'Оплата'
+  sale         FLOAT(5) COMMENT 'Стоимость',
 );
 
 COMMENT ON TABLE Kurs IS 'Курс';
@@ -28,6 +28,10 @@ CREATE TABLE IF NOT EXISTS Student
   address VARCHAR(50) COMMENT 'Адрес',
   phone   VARCHAR(50) COMMENT 'Телефон',
   mail    VARCHAR(50) COMMENT 'электронный адрес',
+  number_doc    VARCHAR(50) COMMENT 'номер зачетки',
+  middle_progress    VARCHAR(50) COMMENT 'электронный адрес',
+  is_active BOOLEAN COMMENT 'возможность записи',
+  kurs_end VARCHAR(50) COMMENT  'прослушаный курс'
 );
 
 COMMENT ON TABLE Student IS 'Студент';
@@ -37,6 +41,7 @@ CREATE TABLE IF NOT EXISTS Pr_kurs
   id         INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
   ocenka     VARCHAR(50) COMMENT 'оценка',
   kurs_id    INTEGER COMMENT 'Связь с таблицей Курс',
+  final_ocenka     VARCHAR(50) COMMENT 'финальная оценка',
   FOREIGN KEY (kurs_id) REFERENCES Kurs (id),
   student_id INTEGER COMMENT 'Связь с таблицей Студент',
   FOREIGN KEY (student_id) REFERENCES Student (id)
@@ -49,3 +54,4 @@ COMMENT ON TABLE Pr_kurs IS 'Прохождение курса';
 CREATE INDEX I_Kurs ON Pr_kurs (kurs_id);
 CREATE INDEX II_Student ON Pr_kurs (student_id);
 CREATE INDEX III_Professor ON Kurs (professor_id);
+
