06ddb18b4f5e4c2edff81ece99841b0f456ad4e0
framzik <faradei33>
2021-03-29 16:27:52 +0300

    Добавил Id в DTO сущность для Rating

--- University/src/main/java/ru/khrebtov/entity/dtoEntity/Rating.java
+++ University/src/main/java/ru/khrebtov/entity/dtoEntity/Rating.java
@@ -1,14 +1,13 @@
 package ru.khrebtov.entity.dtoEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ratings")
 public class Rating {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @Column(name ="study_course_id" )
     private Long studyCourseId;
     @Column(name = "rating")
